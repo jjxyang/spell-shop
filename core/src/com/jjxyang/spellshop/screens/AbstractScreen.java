af0f95ac1e840b6386b9189ca02d32d06404e3cd
Jessie Yang <j.pure.serendipity@gmail.com>
2016-01-12 19:43:41 -0600

    AbstractScreen - Add log events to Screen methods in AbstractScreen - Add creation of Stage instance to AbstractScreen constructor - Adjust resize method to resize viewport of Stage when screen resized - Adjust render method to call Stage's act and draw methods - Adjust dispose method to dispose of Stage
    SplashScreen
    - Remove render method from SplashScreen
    - Move configuration of TextureRegion from show method to resize method
    - Use Image splashImage instead of using SpriteBatch batch
    - Add fade-in/out actions for splashImage within resize method
    - Make SplashScreen switch to MenuScreen

--- core/src/com/jjxyang/spellshop/screens/AbstractScreen.java
+++ core/src/com/jjxyang/spellshop/screens/AbstractScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.jjxyang.spellshop.SpellShop;
 
 /**
@@ -16,37 +17,68 @@ public abstract class AbstractScreen implements Screen {
     protected final SpellShop game;
     protected final BitmapFont font;
     protected final SpriteBatch batch;
+    protected final Stage stage;
 
     public AbstractScreen(SpellShop game) {
         this.game = game;
         this.font = new BitmapFont();
         this.batch = new SpriteBatch();
+        this.stage = new Stage();
+    }
+
+    protected String getName() {
+        return getClass().getSimpleName();
+    }
+
+    // ------------------------------------------
+    // Screen implementation
+    // ------------------------------------------
+
+    @Override
+    public void show() {
+        Gdx.app.log(SpellShop.LOG, "Showing screen: " + getName());
     }
 
     @Override
-    public void show() {}
+    public void resize(int width, int height) {
+        Gdx.app.log(SpellShop.LOG, "Resizing screen: " + getName() + " to: " + width + "x" + height);
 
-    @Override
-    public void resize(int width, int height) {}
+        // resizes the stage
+        stage.getViewport().update(width, height, true);
+    }
 
     @Override
     public void render(float delta) {
         // clears the screen with given RGB color (black)
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // updates and draws stage actors
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
-    public void hide() {}
+    public void hide() {
+        Gdx.app.log(SpellShop.LOG, "Hiding screen: " + getName());
+    }
 
     @Override
-    public void pause() {}
+    public void pause() {
+        Gdx.app.log(SpellShop.LOG, "Pausing screen: " + getName());
+    }
 
     @Override
-    public void resume() {}
+    public void resume() {
+        Gdx.app.log(SpellShop.LOG, "Resuming screen: " + getName());
+    }
 
     @Override
     public void dispose() {
+        Gdx.app.log(SpellShop.LOG, "Disposing screen: " + getName());
+
+        // disposes the collaborators
+        stage.dispose();
         font.dispose();
         batch.dispose();
     }
--- core/src/com/jjxyang/spellshop/screens/SplashScreen.java
+++ core/src/com/jjxyang/spellshop/screens/SplashScreen.java
@@ -1,10 +1,19 @@
 package com.jjxyang.spellshop.screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Scaling;
 import com.jjxyang.spellshop.SpellShop;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.delay;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.fadeIn;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.fadeOut;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
 
 /**
  * Created by Jessie on 1/6/2016.
@@ -12,7 +21,6 @@ import com.jjxyang.spellshop.SpellShop;
 public class SplashScreen extends AbstractScreen {
 
     private Texture splashTexture;
-    private TextureRegion splashTextureRegion;
 
     public SplashScreen(SpellShop game) {
         super(game);
@@ -27,22 +35,40 @@ public class SplashScreen extends AbstractScreen {
 
         // sets linear texture filter to improve stretching
         splashTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-
-        // in image atlas, splash image begins at (4,4)
-        // at upper-left corner with dimensions 2180x1940
-        splashTextureRegion = new TextureRegion(splashTexture, 0, 0, 2180, 1940);
     }
 
     @Override
-    public void render(float delta) {
-        super.render(delta);
+    public void resize(int width, int height) {
+        super.resize(width, height);
 
-        // uses SpriteBatch to draw 2D textures (as defined in AbstractScreen)
-        batch.begin();
+        // clears the stage
+        stage.clear();
 
-        // tells batch to draw region starting at (0,0) of
-        // lower-left corner with the given screen size
-        batch.draw(splashTextureRegion, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        // in image atlas, splash image begins at (4,4)
+        // at upper-left corner with dimensions 2180x1940
+        TextureRegion splashRegion = new TextureRegion(splashTexture, 0, 0, 2180, 1940);
+
+        // creates splash image actor, sets its size
+        Image splashImage = new Image((Drawable) splashRegion, Scaling.stretch, Align.bottomLeft);
+        splashImage.setWidth(width);
+        splashImage.setHeight(height);
+
+        // makes image completely transparent in order to make fade-in effect work correctly
+        splashImage.getColor().a = 0f;
+
+        // configures fade-in/out effect on splash image
+        // after fade-out, switches to MenuScreen
+        splashImage.addAction(sequence(fadeIn(0.75f), delay(1.75f), fadeOut(0.75f),
+            new Action() {
+                public boolean act(float delta) {
+                    game.setScreen(new MenuScreen(game));
+                    return true;    // returning true consumes the event
+                }
+            })
+        );
+
+        // adds actors to stage in correct order
+        stage.addActor(splashImage);
     }
 
     @Override
