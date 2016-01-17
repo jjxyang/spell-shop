a636c6584ee73ce2e90f0a21e679d976ba52c7e6
Jessie Yang <j.pure.serendipity@gmail.com>
2016-01-16 22:16:03 -0600

    AbstractScreen - Lazily load collaborators now (BitmapFont, SpriteBatch, Skin) - Change hide method to call dispose (else, dispose method would never be called) - Redirect input processing to Stage object thru "Gdx.input.setInputProcessor(stage);" in show method - "Reorganize" render method into two parts: processing game logic + drawing result - Clear stage in resize method now
    MenuScreen
    - Add "Welcome" label to resize method
    - Add "Start Game" and "Options" buttons to resize method

--- core/src/com/jjxyang/spellshop/screens/AbstractScreen.java
+++ core/src/com/jjxyang/spellshop/screens/AbstractScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.jjxyang.spellshop.SpellShop;
 
 /**
@@ -15,19 +16,41 @@ import com.jjxyang.spellshop.SpellShop;
  */
 public abstract class AbstractScreen implements Screen {
     protected final SpellShop game;
-    protected final BitmapFont font;
-    protected final SpriteBatch batch;
     protected final Stage stage;
+
+    private BitmapFont font;
+    private SpriteBatch batch;
+    private Skin skin;
 
     public AbstractScreen(SpellShop game) {
         this.game = game;
-        this.font = new BitmapFont();
-        this.batch = new SpriteBatch();
         this.stage = new Stage();
     }
 
     protected String getName() {
         return getClass().getSimpleName();
+    }
+
+    public BitmapFont getFont() {
+        if (font == null) {
+            font = new BitmapFont();
+        }
+        return font;
+    }
+
+    public SpriteBatch getBatch() {
+        if (batch == null) {
+            batch = new SpriteBatch();
+        }
+        return batch;
+    }
+
+    public Skin getSkin() {
+        if (skin == null) {
+            //skin = new Skin(Gdx.files.internal("uiskin.json"), Gdx.files.internal("uiskin.png"));
+            skin = new Skin(); //@TODO fix the above
+        }
+        return skin;
     }
 
     // ------------------------------------------
@@ -37,30 +60,43 @@ public abstract class AbstractScreen implements Screen {
     @Override
     public void show() {
         Gdx.app.log(SpellShop.LOG, "Showing screen: " + getName());
+
+        // sets the input processor
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void resize(int width, int height) {
         Gdx.app.log(SpellShop.LOG, "Resizing screen: " + getName() + " to: " + width + "x" + height);
 
-        // resizes the stage
+        // resizes and clears the stage, centering the camera
         stage.getViewport().update(width, height, true);
+        stage.clear();
     }
 
     @Override
     public void render(float delta) {
+        // (1) processes the game logic --------------------------------
+
+        // updates the actors
+        stage.act(delta);
+
+        // (2) draws the result ----------------------------------------
+
         // clears the screen with given RGB color (black)
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // updates and draws stage actors
-        stage.act(delta);
+        // draws stage actors
         stage.draw();
     }
 
     @Override
     public void hide() {
         Gdx.app.log(SpellShop.LOG, "Hiding screen: " + getName());
+
+        // disposes resources by default
+        dispose();
     }
 
     @Override
@@ -79,7 +115,12 @@ public abstract class AbstractScreen implements Screen {
 
         // disposes the collaborators
         stage.dispose();
-        font.dispose();
-        batch.dispose();
+
+        if (font != null)
+            font.dispose();
+        if (batch != null)
+            batch.dispose();
+        if (skin != null)
+            batch.dispose();
     }
 }
--- core/src/com/jjxyang/spellshop/screens/MenuScreen.java
+++ core/src/com/jjxyang/spellshop/screens/MenuScreen.java
@@ -1,5 +1,7 @@
 package com.jjxyang.spellshop.screens;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.jjxyang.spellshop.SpellShop;
 
 /**
@@ -8,7 +10,45 @@ import com.jjxyang.spellshop.SpellShop;
  * Menu screen of the Spell Shop game @TODO
  */
 public class MenuScreen extends AbstractScreen {
+    // sets up the dimensions of the menu buttons
+    private static final float BUTTON_WIDTH = 400f;
+    private static final float BUTTON_HEIGHT = 80f;
+    private static final float BUTTON_SPACING = 15f;
+
     public MenuScreen(SpellShop game) {
         super(game);
+    }
+
+    // ------------------------------------------
+    // Screen implementation
+    // ------------------------------------------
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        final float buttonX = (width - BUTTON_WIDTH) / 2;
+        float currentY = 300f;
+
+        // label "welcome"
+        Label welcomeLabel = new Label("Welcome to your very own Spell Shop!", getSkin());
+        welcomeLabel.setX((width - welcomeLabel.getWidth()) / 2);
+        welcomeLabel.setY(currentY + 100);
+        stage.addActor(welcomeLabel);
+
+        // button "start game"
+        TextButton startGameButton = new TextButton("Start Game", getSkin());
+        startGameButton.setX(buttonX);
+        startGameButton.setY(currentY);
+        startGameButton.setWidth(BUTTON_WIDTH);
+        startGameButton.setHeight(BUTTON_HEIGHT);
+        stage.addActor(startGameButton);
+
+        // button "options"
+        TextButton optionsButton = new TextButton("Options", getSkin());
+        optionsButton.setX(buttonX);
+        optionsButton.setY(currentY -= BUTTON_HEIGHT + BUTTON_SPACING);
+        optionsButton.setWidth(BUTTON_WIDTH);
+        optionsButton.setHeight(BUTTON_HEIGHT);
+        stage.addActor(optionsButton);
     }
 }
